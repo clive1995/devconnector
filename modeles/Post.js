edbddef8df830b81911b0496b22bf4cb78ffac51
clive <clive@opspl.com>
2020-09-08 16:46:44 +0530

    created schema for post model

---
+++ modeles/Post.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose')
+
+const PostSchema = new mongoose.Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Post = mongoose.model('Post', PostSchema, 'post');
+
+module.exports = Post
